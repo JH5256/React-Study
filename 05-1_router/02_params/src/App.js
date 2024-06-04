0113e93d57809bc7e7b43c1a6f7035237032038e
JH5256 <skdltmvhs2@naver.com>
2024-06-04 15:35:04 +0900

    react study

--- 05-1_router/02_params/src/App.js
+++ 05-1_router/02_params/src/App.js
@@ -1,24 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './layouts/Layout';
-import Main from './pages/Main';
-import About from './pages/About';
-import Menu from './pages/Menu';
-import MenuDetails from './pages/MenuDetails';
-import MenuSearchResult from './pages/MenuSearchResult';
+import Layout from "./layouts/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import MenuDetails from "./pages/MenuDetails";
+import MenuSearchResult from "./pages/MenuSearchResult";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
-          <Route path='about' element={<About />} />
-          <Route path='menu'>
-            <Route index element={<Menu />}/>
-            <Route path=":menuCode" element={<MenuDetails />}/>
-            <Route path='search' element={<MenuSearchResult />}/>
+          <Route path="about" element={<About />} />
+          <Route path="menu">
+            <Route index element={<Menu />} />
+            <Route path=":menuCode" element={<MenuDetails />} />
+            <Route path="search" element={<MenuSearchResult />} />
           </Route>
         </Route>
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
